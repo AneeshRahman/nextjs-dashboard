51ebe6354277cbeeebc02f47113e879e1db65af8
Aneesh165 <aneeshmp165@gmail.com>
2025-05-24 19:45:00 +0530

    h

--- app/dashboard/invoice/page.tsx
+++ app/dashboard/invoice/page.tsx
@@ -1,8 +1,12 @@
 export default function Page() {
   return (
     <main className="h-screen ml-56 mt-56">
-      <div className="w-96 h-96 bg-red-500 rounded-lg">
-
+      <div className="w-96 h-96 bg-red-500 rounded-lg text-white">
+        <h1>heading</h1>
+        <h1>heading</h1>
+        <h1>heading</h1>
+        <h1>heading</h1>
+        <h1>heading</h1>
       </div>
     </main>
   )
