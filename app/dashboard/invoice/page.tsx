14aff3674822ffc501e466ea7144950f02165233
Aneesh165 <aneeshmp165@gmail.com>
2025-05-30 15:30:34 +0530

    h removed

--- app/dashboard/invoice/page.tsx
+++ app/dashboard/invoice/page.tsx
@@ -4,9 +4,7 @@ export default function Page() {
       <div className="w-96 h-96 bg-red-500 rounded-lg text-white">
         <h1>heading</h1>
         <h1>heading</h1>
-        <h1>heading</h1>
-        <h1>heading</h1>
-        <h1>heading</h1>
+        <h1>heading</h1>        
       </div>
     </main>
   )
